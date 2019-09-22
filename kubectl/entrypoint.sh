2de39bd3dcc20dedf963eb3d75f94fec1f7f8d19
Ramene Anthony <anthonyramene@gmail.com>
2019-09-22 00:27:23 -0400

    initial migrated workflow

--- kubectl/entrypoint.sh
+++ kubectl/entrypoint.sh
@@ -8,18 +8,18 @@ if [ ! -z "${GITHUB_WORKFLOW}" ]; then
     KUBECTL_CMD="kubectl $*"
     PULUMI_SHARED="$GITHUB_WORKSPACE/.pulumi"
 
-    if [ ! -e ${PULUMI_SHARED}/aws-auth.yml ]; then
+    if [ ! -e "${PULUMI_SHARED}/aws-auth.yml" ]; then
 
         aws configure set default.region us-east-1
 
         if [ ! -z "$PULUMI_SHARED" ]; then
-            cd $PULUMI_SHARED
+            cd "$PULUMI_SHARED"
             
             wget -qO- -O aws-auth.yml \
                 https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/aws-auth-cm.yaml
 
-            export KUBECONFIG=kubeconfig
-            export EKS_WORKER_ROLE=$(cat output)
+            KUBECONFIG=kubeconfig
+            EKS_WORKER_ROLE=$(cat output)
 
             sed -i -e "s#<ARN of instance role (not instance profile)>#${EKS_WORKER_ROLE}#g" aws-auth.yml
         
@@ -28,4 +28,4 @@ if [ ! -z "${GITHUB_WORKFLOW}" ]; then
     fi
 fi
 
-sh -c "$KUBECTL_CMD" | tee $OUTPUT_FILE
+sh -c "$KUBECTL_CMD" | tee "$OUTPUT_FILE"
