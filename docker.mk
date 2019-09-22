e964d3b4c6ea890ece7cd27c8f56d4ce790dbdc1
Ramene Anthony <anthonyramene@gmail.com>
2019-09-22 01:01:47 -0400

    initial migrated workflow

--- docker.mk
+++ docker.mk
@@ -13,5 +13,5 @@ docker-tag: ## Tag the docker image using the tag script.
 	tag $(IMAGE_NAME) $(DOCKER_REPO)/$(IMAGE_NAME) --no-latest
 
 .PHONY: docker-publish
-docker-publish: docker-tag ## Publish the image and tags to a repository.
+docker-publish: ## Publish the image and tags to a repository.
 	docker push $(DOCKER_REPO)/$(IMAGE_NAME)
